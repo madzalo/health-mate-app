a5687cf7dda33354a13c70ceb86e00233d4ab5e7
madzalo <madzaloresten8@protonmail.com>
2024-06-01 13:50:59 +0200

    customized logos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.dhis2"
+        applicationId = "com.healthmate"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.sdk.get().toInt()
         versionCode = libs.versions.vCode.get().toInt()
